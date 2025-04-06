21d60670e0658f5671def899fbd86b1074fe4bc6
huanghong <bbc6bae9@gmail.com>
2025-04-06 22:40:19 +0800

    feat(login): 新增输入框

--- Sources/ClientHub/Clients/EmbyClient/EmbyLoginView.swift
+++ Sources/ClientHub/Clients/EmbyClient/EmbyLoginView.swift
@@ -14,59 +14,77 @@ struct EmbyLoginView: View {
 
     @MainActor var onLogin: (AuthenticationResponse) -> Void
     
+    @State private var name: String = ""
     @State private var host: String = ""
+    @State private var port: String = ""
     @State private var username: String = ""
     @State private var password: String = ""
     @State private var errorMessage: String = ""
     
     var body: some View {
-        VStack {
-            TextField("host", text: $host)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            TextField("Username", text: $username)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            TextField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            if !errorMessage.isEmpty {
-                Text(errorMessage)
-                    .foregroundColor(.red)
-                    .padding()
-            }
-            
-            Button(action: {
-                
-                presentationMode.wrappedValue.dismiss()
-
-                Task { @MainActor in
-                    do {
-                        guard let url = URL(string: host) else {
-                            errorMessage = "Invalid URL"
-                            return
-                        }
-                        let embyClient = EmbyKit.EmbyClient(baseURL: url)
-                        let authenticationResponse = try await embyClient.authenticate(username: username, password: password)
-                        onLogin(authenticationResponse)
-                    } catch {
-                        errorMessage = "Authentication failed: \(error.localizedDescription)"
+        NavigationView {
+            ScrollView {
+                VStack {
+                    TextField("name", text: $name)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    TextField("host", text: $host)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    TextField("port", text: $port)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    TextField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    if !errorMessage.isEmpty {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .padding()
                     }
-                }
-            }) {
-                Text("Login")
-                    .frame(maxWidth: .infinity)
+                    
+                    Text("Moviers使用Emby官方API直连媒体服务器，使用过程中不会刮削扫描媒体库文件")
+                        .foregroundColor(.secondary)
+                    
+                    Button(action: {
+                        
+                        presentationMode.wrappedValue.dismiss()
+
+                        Task { @MainActor in
+                            do {
+                                guard let url = URL(string: host) else {
+                                    errorMessage = "Invalid URL"
+                                    return
+                                }
+                                let embyClient = EmbyKit.EmbyClient(baseURL: url)
+                                let authenticationResponse = try await embyClient.authenticate(username: username, password: password)
+                                onLogin(authenticationResponse)
+                            } catch {
+                                errorMessage = "Authentication failed: \(error.localizedDescription)"
+                            }
+                        }
+                    }) {
+                        Text("Login")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
                     .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
+                }
+                .padding()
             }
-            .padding()
+            .navigationTitle("添加Emby")
         }
-        .padding()
     }
 }
 
